55257da4fff8fced27309407dbd11a7e90ad5132
Rangga Ajie Febriansyah <ranggafebriansyah19@gmail.com>
2026-03-06 10:42:34 +0700

    Refactor lamp UI using TailwindCSS and Flowbite toggle
    Refactor lamp control interface:
    - Add TailwindCSS and Flowbite
    - Replace buttons with toggle switch
    - Improve layout using flex and centered container

---
+++ saklar.js
@@ -0,0 +1,30 @@
+function saklar(action, lamp) {
+    let lampu1 = document.getElementById("lampu1");
+    let lampu2 = document.getElementById("lampu2");
+    let lampu3 = document.getElementById("lampu3");
+
+    if (action == "on") {
+        //nyala
+        if (lamp == 1) {
+            lampu1.src = "assets/images/on.gif";
+        }
+        if (lamp == 2) {
+            lampu2.src = "assets/images/on.gif";
+        }
+        if (lamp == 3) {
+            lampu3.src = "assets/images/on.gif";
+        }
+    }
+       if (action == "off") {
+        //mati 
+        if (lamp == 1) {
+            lampu1.src = "assets/images/off.gif";
+        }
+        if (lamp == 2) {
+            lampu2.src = "assets/images/off.gif";
+        }
+        if (lamp == 3) {
+            lampu3.src = "assets/images/off.gif";
+        }
+}
+}
